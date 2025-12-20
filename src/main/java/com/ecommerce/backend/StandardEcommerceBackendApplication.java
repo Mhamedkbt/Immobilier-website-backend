2c25268b747ff9abe952d13e72592949117dbe3c
mhamed kbt <mhamedkhebit@gmail.com>
2025-12-20 17:37:10 +0100

    m

--- src/main/java/com/ecommerce/backend/StandardEcommerceBackendApplication.java
+++ src/main/java/com/ecommerce/backend/StandardEcommerceBackendApplication.java
@@ -2,13 +2,14 @@ package com.ecommerce.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling; // 1. Added import
 
 @SpringBootApplication
+@EnableScheduling // 2. Added annotation to enable the timer
 public class StandardEcommerceBackendApplication {
 
-	public static void main(String[] args) {
-
+    public static void main(String[] args) {
         SpringApplication.run(StandardEcommerceBackendApplication.class, args);
-	}
+    }
 
 }
